33b15f26ad5da49ee4bc5c10b4894791ca8c6c9a
Hamed Saeedi <hameds@gmail.com>
2019-04-21 12:01:24 +0430

    initial commit

---
+++ Araye.Code.Cqrs.WebApi/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Araye.Code.Cqrs.WebApi
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public abstract class BaseApiController : Controller
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+    }
+}
